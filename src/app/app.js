26bfbe0958eef350ad8467b0a44f6e0fd53d3688
Gregory Zhou <bottlezz2002@gmail.com>
2015-06-22 17:36:46 -0700

    Learning express Js

---
+++ src/app/app.js
@@ -0,0 +1,5 @@
+var httpServer = require("./httpServer").listen(8000, function(){
+	var host=httpServer.address().address;
+	var port= httpServer.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+})
